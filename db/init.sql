fe0171be66146166d9c24a1f8a5ea779750f3ec8
quiloos39 <quiloos39@gmail.com>
2021-03-28 19:48:47 +0300

    Added not null to fields.

--- db/init.sql
+++ db/init.sql
@@ -1,13 +1,13 @@
--- I assumed productID is primary key and assumed other variables could be NULL
+-- I assumed productID is primary key.
 -- If i had time i would probably added another column called ID and make it primary key
 -- so that productID could be changed but again i wasn't given any constrains.
 
 CREATE DATABASE test;
 
 CREATE TABLE test.parameters(
-    productID int AUTO_INCREMENT,
-    productName varchar(32),
-    productDescription varchar(255),
+    productID int AUTO_INCREMENT NOT NULL,
+    productName varchar(32) NOT NULL,
+    productDescription varchar(255) NOT NULL,
     PRIMARY KEY (productID)
 );
 
